46dc6a40d5199c8b385a28806ee22bc99bceddf9
Chua Eu Jing <chuaeujing@gmail.com>
2016-02-21 01:25:48 +0800

    Graphs working

---
+++ webserver/chrack/Graph.jsx
@@ -0,0 +1,110 @@
+LiveGraph = React.createClass({
+    propTypes: {
+        graphID: React.PropTypes.string.isRequired,
+        trendDatas: React.PropTypes.array.isRequired, // [{class, data, colour}, {...}, ...]
+        outerWidth: React.PropTypes.number.isRequired,
+        outerHeight: React.PropTypes.number.isRequired,
+        margin: React.PropTypes.object.isRequired,
+        yMin: React.PropTypes.number.isRequired,
+        yMax: React.PropTypes.number.isRequired
+    },
+
+    componentDidMount: function() {
+        let p = this.props;
+        let width = p.outerWidth - p.margin.left - p.margin.right;
+        let height = p.outerHeight - p.margin.top - p.margin.bottom;
+
+        let el = ReactDOM.findDOMNode(this);
+        let svg = d3.select(el)
+                    .append("svg")
+                        .attr("id", p.graphID)
+                        .attr("width", p.width)
+                        .attr("height", p.height)
+                        .attr("preserveAspectRatio", "xMinYMin meet")
+                        .attr("viewBox", "0 0 " + p.outerWidth + " " + p.outerHeight)
+                    .append("g")
+                        .attr("transform", "translate(" + p.margin.left + ", " + p.margin.top + ")");
+
+        svg.append("defs")
+            .append("clipPath")
+                .attr("id", "clip")
+            .append("rect")
+                .attr("width", width)
+                .attr("height", height);
+
+        let realXMin = d3.min(d3.min(p.trendDatas, (trendData) => trendData.data), (d) => d.x);
+
+        let realXMax = d3.max(d3.max(p.trendDatas, (trendData) => trendData.data), (d) => d.x);
+
+        this.xScale = d3.time.scale()
+                        .domain([realXMin, realXMax])
+                        .range([0, width]);
+
+        this.yScale = d3.scale.linear()
+                        .domain([p.yMin, p.yMax])
+                        .range([height, 0]);
+
+        this.lines = p.trendDatas.map((trendData) => {
+            return d3.svg.line()
+                    .x((d, i) => this.xScale(d.x))
+                    .y((d, i) => this.yScale(d.y))
+                    .interpolate("cardinal");
+        });
+
+        this.xAxis = d3.svg.axis()
+                        .scale(this.xScale)
+                        .orient("bottom");
+
+        this.yAxis = d3.svg.axis()
+                        .scale(this.yScale)
+                        .orient("left");
+
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(this.xAxis);
+
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(this.yAxis);
+
+        let viewport = svg.append("g")
+                        .attr("clip-path", "url(#clip)");
+
+        let paths = p.trendDatas.map((trendData, dataIndex) => {
+            return viewport.append("path")
+                    .datum(trendData.data)
+                    .attr("class", "line " + trendData.name)
+                    .attr("stroke", trendData.color)
+                    .attr("d", this.lines[dataIndex]);
+        });
+
+        this.updateGraph(p);
+    },
+
+    componentWillUpdate(nextProps) {
+        this.updateGraph(nextProps);
+    },
+
+    updateGraph(props) {
+        let svg = d3.select("#" + props.graphID).transition();
+
+        let realXMin = d3.min(d3.min(props.trendDatas, (trendData) => trendData.data), (d) => d.x);
+
+        let realXMax = d3.max(d3.max(props.trendDatas, (trendData) => trendData.data), (d) => d.x);
+
+        this.xScale.domain([realXMin, realXMax]);
+        this.yScale.domain([props.yMin, props.yMax]);
+
+        props.trendDatas.map((trendData, dataIndex) => {
+            svg.select(".line." + trendData.name)
+                .duration(1000)
+                .ease("linear")
+                .attr("d", this.lines[dataIndex](trendData.data));
+        });
+    },
+
+    render() {
+        return <div className="live-graph"></div>;
+    }
+});
--- webserver/chrack/Personal.jsx
+++ webserver/chrack/Personal.jsx
@@ -3,16 +3,22 @@ Personal = React.createClass({
     mixins: [ReactMeteorData],
 
     getMeteorData() {
+        let currentUser = Meteor.user();
+        let userChairObjectID = currentUser && new Meteor.Collection.ObjectID(currentUser.profile.chair_id) || {};
+
         return {
             currentUser: Meteor.user(),
-            chairs: Chairs.find({}).fetch()
+            chairs: Chairs.find({}).fetch(),
+            user_chair: Chairs.findOne(userChairObjectID)
         };
     },
 
     renderChairs() {
-        return this.data.chairs.map((chair) => {
-            return <option key={chair._id} value={chair._id}>{ chair.name }</option>;
-        });
+        if (this.data.chairs) {
+            return this.data.chairs.map((chair) => {
+                return <option key={chair._id} value={chair._id}>{ chair.name }</option>;
+            });
+        }
     },
 
     handleChairChange(event) {
@@ -32,7 +38,7 @@ Personal = React.createClass({
                         <h1>Hi {this.data.currentUser.username}, you are sitting at:</h1>
                         <select
                             className="form-control"
-                            value={this.data.currentUser.profile.chair_id}
+                            value={this.data.currentUser && this.data.currentUser.profile.chair_id}
                             onChange={this.handleChairChange}>
                             <option>Select One</option>
                             {this.renderChairs()}
@@ -40,6 +46,36 @@ Personal = React.createClass({
                     </div>
                     : <h1> Please Log In </h1>
                 }
+                {this.data.user_chair ?
+                <div className="graphs">
+                    <div className="panel panel-primary">
+                        <div className="panel-heading">Movement</div>
+                        <div className="panel-body>">
+                            <LiveGraph
+                                graphID="movementGraph"
+                                trendDatas={this.data.user_chair.values}
+                                yMin={0.0}
+                                yMax={1.0}
+                                outerWidth={640}
+                                outerHeight={160}
+                                margin={{left:40, right:20, top:20, bottom:20}} />
+                        </div>
+                    </div>
+                    <div className="panel panel-primary">
+                        <div className="panel-heading">Spin</div>
+                        <div className="panel-body>">
+                            <LiveGraph
+                                graphID="spinGraph"
+                                trendDatas={this.data.user_chair.values}
+                                yMin={0.0}
+                                yMax={1.0}
+                                outerWidth={640}
+                                outerHeight={160}
+                                margin={{left:40, right:20, top:20, bottom:20}} />
+                        </div>
+                    </div>
+                </div>
+                : ""}
             </div>
         );
     }
